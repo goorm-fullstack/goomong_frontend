b6be9ae979b72f42d4b6977cf19db24918b728cf
SunWoong <lovering1771@naver.com>
2023-11-22 00:06:13 +0900

    20231121

--- src/pages/Chatting/ChattingRoom/ChattingRoomStyles.ts
+++ src/pages/Chatting/ChattingRoom/ChattingRoomStyles.ts
@@ -6,5 +6,16 @@ export const ChattingRoomStyles = styled.div`
     height: 700px;
     border-radius: 8px;
     border: 1px solid #dbdee2;
+    overflow: auto;
+
+    .all-room {
+      color: #101c33;
+      font-size: 13px;
+      font-weight: 600;
+      letter-spacing: -0.04em;
+      padding: 12px 0 14px 11px;
+    }
+    .room-list {
+    }
   }
 `;
