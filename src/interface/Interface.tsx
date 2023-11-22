0cc4ee863b215069f8d9e4b6d6afbfb8f76edbcb
jeosong <13bitadc@naver.com>
2023-11-22 19:01:09 +0900

    백엔드 포인트부분 연동

--- src/interface/Interface.tsx
+++ src/interface/Interface.tsx
@@ -169,3 +169,16 @@ export interface ResponseData {
   delDate: Date;
   pageInfo: PageInfoData;
 }
+
+export interface Point {
+  totalPoint: number;
+  expiringPoint: number;
+}
+
+export interface PointHistory {
+  type: string;
+  amount: number;
+  description: string;
+  orderNumber: string;
+  regDate: Date;
+}
