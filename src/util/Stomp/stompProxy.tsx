7557c9c2a9441135658380c2fc6611f6b7060fd6
WhiteKIM <9rudrb40@naver.com>
2023-11-22 10:04:21 +0900

    2023.11.22 upload

---
+++ src/util/Stomp/stompProxy.tsx
@@ -0,0 +1,5 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app: any) => {
+  app.use('/ws', createProxyMiddleware({ target: 'http://localhost:8080', ws: true }));
+};
