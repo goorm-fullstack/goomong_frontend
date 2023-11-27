e8a07cd29656615a8b80f6ae782daba2864d137c
jeosong <13bitadc@naver.com>
2023-11-27 19:16:31 +0900

    chatting

--- src/util/Stomp/stompProxy.tsx
+++ src/util/Stomp/stompProxy.tsx
@@ -1,7 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = (app: any) => {
-  app.use('/ws', createProxyMiddleware({ target: 'https://keba1da28866ea.user-app.krampoline.com', ws: true }));
-};
+// module.exports = (app: any) => {
+//   app.use('/ws', createProxyMiddleware({ target: 'https://keba1da28866ea.user-app.krampoline.com', ws: true }));
+// };
 
-export {};
+// export {};
